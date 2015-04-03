734a60834bcd21e20e2942cf8b6ae1511aa0e43d
Stefan Langenmaier <stefan.langenmaier@savoirfairelinux.com>
2015-04-03 16:56:42 -0400

    file copies all linked libraries to a specific path

---
+++ copy-recursive-ll.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+if [ "$#" -ne 2 ] ; then
+    echo "need executable and dest"
+fi
+
+EXECUTABLE=$1
+DEST_DIR=$2
+
+if [ -e "$EXECUTABLE" ] && [ -f "$EXECUTABLE" ] && [ -e "$DEST_DIR" ] && [ -d "$DEST_DIR" ]; then
+    if ! [ -e "$DEST_DIR$EXECUTABLE" ] ; then
+        ldd "$EXECUTABLE" | sed 's/[^\/]*\/\(.*\) .*/\/\1/' | \
+        while read library
+        do
+            echo "library: " $library
+            bash $0 $library $DEST_DIR
+        done
+        echo "copying executable $EXECUTABLE"
+        cp "$EXECUTABLE" "$DEST_DIR$EXECUTABLE"
+        
+    fi
+fi
+
