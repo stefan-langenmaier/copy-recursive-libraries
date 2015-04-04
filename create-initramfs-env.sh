a5d9af76e5d9b2aebb039ac9de3b1cf63e996c7f
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2015-04-04 14:11:05 -0400

    added a  default init script added a script that generates the initramfs with the necessary entries

---
+++ create-initramfs-env.sh
@@ -0,0 +1,16 @@
+mkdir -p /usr/src/initramfs/{bin,dev,etc,lib,lib64,mnt/root,proc,root,sbin,sys,usr/lib64}
+cp -a /dev/{null,console,tty,sda3} /usr/src/initramfs/dev/
+cp -a /bin/busybox /usr/src/initramfs/bin/busybox
+cp -a init /usr/src/initramfs/init
+chmod +x /usr/src/initramfs/init
+
+cp -a /dev/{urandom,random} /usr/src/initramfs/dev
+bash coppy-recursive-ll.sh /sbin/cryptsetup /usr/src/initramfs/sbin/cryptsetup
+
+# install a shell
+cd /usr/src/initramfs
+ln bin/busybox bin/sh
+
+# for a seperate file
+cd /usr/src/initramfs
+find . -print0 | cpio --null -ov --format=newc | gzip -9 > /boot/custom-initramfs.cpio.gz
